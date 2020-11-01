f37273884e42f2320a8a2a0511185cb7ac37fbaf
Kevin Lee <klee03509@gmail.com>
2020-11-01 11:51:15 -0800

    Add 3D plane implementation

---
+++ src/main/java/trace/hittable/Plane.java
@@ -0,0 +1,42 @@
+package trace.hittable;
+
+import trace.geometry.Ray3;
+import trace.geometry.Vec3;
+import trace.material.Material;
+
+/**
+ * A hittable 3D plane.
+ *
+ * @author Kevin Lee
+ */
+public class Plane extends AbstractHittable {
+
+    private final Vec3 origin;
+    private final Vec3 normal;
+
+    public Plane(Vec3 origin, Vec3 normal, Material material) {
+        super(material);
+
+        this.origin = origin;
+        this.normal = normal.unit();
+    }
+
+    @Override
+    public boolean hit(double tMin, double tMax, Ray3 ray, HitRecord rec) {
+        double d = normal.dot(ray.getDirection());
+
+        if (d > 1e-6) {
+            double t = origin.sub(ray.getOrigin()).dot(normal) / d;
+
+            return rec.record(t, tMin, tMax, ray, this);
+        }
+
+        return false;
+    }
+
+    @Override
+    public Vec3 getSurfaceNormal(Vec3 point) {
+        return normal;
+    }
+
+}
